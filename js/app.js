2db1e2eb88189cb4d368234aebbfb9cfd03b9baf
Gil <gilfpf@hotmail.com>
2018-10-14 12:41:40 +0100

    onExtenssions

--- js/app.js
+++ js/app.js
@@ -2,27 +2,21 @@ document.addEventListener('DOMContentLoaded',() => {
   console.log('ready to go')
 
 
-  // listeners
-  // #new-item-form poinst to the id in HTML
   const newItemform = document.querySelector('#new-item-form');
   newItemform.addEventListener('submit', NewItemFormSubmit);
 
 
   const deleteAllButton = document.querySelector('#delete-all')
   deleteAllButton.addEventListener ('click', HandleDeleteAll);
-  // #delete-all poits to DeleteAll button in HTML
 
 })
 
-// main function, points to listener(NewItemForm)
 
 const NewItemFormSubmit = function (event){
   event.preventDefault();
 
-//reads input from the browser?
   const readingListItem = createReadingListItem(event.target);
 
-// submits input from the browser
   const readingList = document.querySelector('#reading-list');
   readingList.appendChild(readingListItem);
 
@@ -34,17 +28,22 @@ const createReadingListItem = function (form) {
   const readingListItem = document.createElement('li');
   readingListItem.classList.add('reading-list-item');
 
-  const title = document.createElement('h2');
-  title.textContent = form.title.value;
-  readingListItem.appendChild(title);
+  const first_name = document.createElement('h4');
+  first_name.textContent = form.first_name.value;
+  readingListItem.appendChild(first_name);
 
-  const author = document.createElement('h3');
-  author.textContent = form.author.value;
-  readingListItem.appendChild(author);
+  const last_name = document.createElement('h4');
+  last_name.textContent = form.last_name.value;
+  readingListItem.appendChild(last_name);
 
-  const category =document.createElement('p');
-  category.textContent = form.category.value;
-  readingListItem. appendChild(category);
+  const sport =document.createElement('h4');
+  sport.textContent = form.sport.value;
+  readingListItem. appendChild(sport);
+
+  const gender = document.createElement('h4');
+  gender.textContext = form.gender.value;
+  readingListItem.appendChild(gender);
+
   return readingListItem;
 
 }
